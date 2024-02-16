a1ec8d327e79c30f505fce96dbd0ee396734f465
Leonardo Anunciacao <anunciacao.leo@gmail.com>
2024-02-16 14:09:53 -0300

    ConsumeApi Service

---
+++ src/main/java/br/com/bananarosasaudavel/restaurantmanagement/Main.java
@@ -0,0 +1,18 @@
+package br.com.bananarosasaudavel.restaurantmanagement;
+
+import br.com.bananarosasaudavel.restaurantmanagement.services.ConsumeApi;
+
+import java.util.Scanner;
+
+public class Main {
+    public Scanner scanner = new Scanner(System.in);
+
+    public void displayMenu(){
+        System.out.println("Enter the desired postal code (numbers only): ");
+        String postalCode = scanner.nextLine();
+
+        ConsumeApi consumeApi = new ConsumeApi();
+        String test = consumeApi.fetchApi("https://viacep.com.br/ws/" + postalCode + "/json/");
+        System.out.println(test);
+    }
+}
--- src/main/java/br/com/bananarosasaudavel/restaurantmanagement/RestaurantManagementApplication.java
+++ src/main/java/br/com/bananarosasaudavel/restaurantmanagement/RestaurantManagementApplication.java
@@ -1,5 +1,6 @@
 package br.com.bananarosasaudavel.restaurantmanagement;
 
+import br.com.bananarosasaudavel.restaurantmanagement.services.ConsumeApi;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,6 +14,7 @@ public class RestaurantManagementApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-
+		Main main = new Main();
+		main.displayMenu();
 	}
 }
